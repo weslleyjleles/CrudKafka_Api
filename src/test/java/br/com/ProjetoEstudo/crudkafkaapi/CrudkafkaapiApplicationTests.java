51a1c8a45f74db31f97ba0c36e431f79f153de99
weslleyjleles <62043182+weslleyjleles@users.noreply.github.com>
2022-12-13 11:56:38 -0300

    subindo projeto

---
+++ src/test/java/br/com/ProjetoEstudo/crudkafkaapi/CrudkafkaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ProjetoEstudo.crudkafkaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudkafkaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
