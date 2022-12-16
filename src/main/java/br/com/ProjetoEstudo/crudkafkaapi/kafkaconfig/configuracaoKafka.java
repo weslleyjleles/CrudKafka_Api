9dd54dc7adc048edb0bf8a447d132416c45e0bcd
weslleyjleles <62043182+weslleyjleles@users.noreply.github.com>
2022-12-16 14:35:39 -0300

    atualizar brench

--- src/main/java/br/com/ProjetoEstudo/crudkafkaapi/kafkaconfig/configuracaoKafka.java
+++ src/main/java/br/com/ProjetoEstudo/crudkafkaapi/kafkaconfig/configuracaoKafka.java
@@ -23,63 +23,66 @@ import java.util.Map;
 @EnableKafka
 public class configuracaoKafka {
 
-    private <T> Map<String, Object> getProps(Class<T> consumerTypeClass) {
-        Map<String, Object> props = new HashMap<>();
+	private <T> Map<String, Object> getProps(Class<T> consumerTypeClass) {
+		Map<String, Object> props = new HashMap<>();
 
-        // Obrigatorias
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        // JSONSERIALIZER
-        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, consumerTypeClass);
+		// Obrigatorias
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+		// JSONSERIALIZER
+		props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+		props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+		props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, consumerTypeClass);
 
-        // Opcionais
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 30000);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		// Opcionais
+		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 30000);
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        return props;
-    }
+		return props;
+	}
 
-    @Bean(name = "userContainerFactory")
-    public ConcurrentKafkaListenerContainerFactory<Integer, Object> userContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<Integer, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+	@Bean(name = "userContainerFactory")
+	public ConcurrentKafkaListenerContainerFactory<Integer, Object> userContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<Integer, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 
-        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(getProps(UsuarioMessage.class)));
-        factory.getContainerProperties().setMissingTopicsFatal(false);
-        factory.getContainerProperties().setSyncCommits(true);
+		factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(getProps(UsuarioMessage.class)));
+		factory.getContainerProperties().setMissingTopicsFatal(false);
+		factory.getContainerProperties().setSyncCommits(true);
 
-        return factory;
-    }
+		return factory;
+	}
 
-    @Bean(name = "CommentContainerFactory")
-    public ConcurrentKafkaListenerContainerFactory<Integer, Object> CommentContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<Integer, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+	@Bean(name = "CommentContainerFactory")
+	public ConcurrentKafkaListenerContainerFactory<Integer, Object> CommentContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<Integer, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 
-        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(getProps(ComentarioMessage.class)));
-        factory.getContainerProperties().setMissingTopicsFatal(false);
-        factory.getContainerProperties().setSyncCommits(true);
+		factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(getProps(ComentarioMessage.class)));
+		factory.getContainerProperties().setMissingTopicsFatal(false);
+		factory.getContainerProperties().setSyncCommits(true);
 
-        return factory;
-    }
+		return factory;
+	}
 
-    @Bean(name = "LikeContainerFactory")
-    public ConcurrentKafkaListenerContainerFactory<Integer, Object> LikeContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<Integer, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+	@Bean(name = "LikeContainerFactory")
+	public ConcurrentKafkaListenerContainerFactory<Integer, Object> LikeContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<Integer, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 
-        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(getProps(LikeMessage.class)));
+		factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(getProps(LikeMessage.class)));
+		factory.getContainerProperties().setMissingTopicsFatal(false);
+		factory.getContainerProperties().setSyncCommits(true);
+		return factory;
+	}
 
+	@Bean(name = "PostContainerFactory")
+	public ConcurrentKafkaListenerContainerFactory<Integer, Object> PostContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<Integer, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 
-    @Bean(name = "PostContainerFactory")
-    public ConcurrentKafkaListenerContainerFactory<Integer, Object> PostContainerFactory(){
-        ConcurrentKafkaListenerContainerFactory<Integer, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(getProps(PostMessage.class)));
+		factory.getContainerProperties().setMissingTopicsFatal(false);
+		factory.getContainerProperties().setSyncCommits(true);
 
-        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(getProps(PostMessage.class)));
-
-        factory.getContainerProperties().setMissingTopicsFatal(false);
-        factory.getContainerProperties().setSyncCommits(true);
-
-        return factory;
+		return factory;
+	}
 
 }
