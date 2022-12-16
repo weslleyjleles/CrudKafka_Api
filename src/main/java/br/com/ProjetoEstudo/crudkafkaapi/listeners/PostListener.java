73ae765c29e1c77e4c40d88b01bbc0eea7fe3a2c
MIR4NDINH4 <114924233+MIR4NDINH4@users.noreply.github.com>
2022-12-16 10:26:49 -0300

    Adicionando o listener de Post

---
+++ src/main/java/br/com/ProjetoEstudo/crudkafkaapi/listeners/PostListener.java
@@ -0,0 +1,18 @@
+package br.com.ProjetoEstudo.crudkafkaapi.listeners;
+
+import br.com.ProjetoEstudo.crudkafkaapi.messages.PostMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostListener {
+
+    private final Logger logger = LoggerFactory.getLogger(ComentarioListener.class);
+
+    @KafkaListener(topics = "Postagem", groupId = "CONSUMER_APPLICATION_EXAMPLE_GROUP", containerFactory = "PostContainerFactory")
+    public void PostListener(PostMessage message) {
+        logger.info("\n Post Kafka Listener received value: \n {}", message.getDataHoraPublicacao());
+    }
+}
---
+++ src/main/java/br/com/ProjetoEstudo/crudkafkaapi/messages/PostMessage.java
@@ -0,0 +1,23 @@
+package br.com.ProjetoEstudo.crudkafkaapi.messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostMessage {
+    private Integer idPublicacao;
+    private Integer idUsuario;
+    private String tituloPublicacao;
+    private String textoPublicacao;
+    private Integer visualisacaoPublicacao;
+    private Integer likePublicacao;
+    private LocalDateTime dataHoraPublicacao = LocalDateTime.now();
+    private String acaoPublicacao;
+}
